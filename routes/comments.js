91aee7cecbdfc1126b3c49a30f03d877681e51b8
Mike Tsou <mr.tsou@gmail.com>
2017-07-23 23:31:22 -0700

    Create comments files for model and routes

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+const express = require("express");
+      middleware = require("../middleware/index");
+      router = express.Router();
+// MODELS
+      Comment = require("../models/comment");
+      Post = require("../models/post");
+
+
+
+
+
+
+
+module.exports = router;
